e7672e88d397fea1288ab0392304d0ce89904276
wjc2008 <wjc2008@live.cn>
2019-11-26 07:48:38 +0800

    阿嘎发

--- pythonbox/excise.py
+++ pythonbox/excise.py
@@ -1,1 +1,7 @@
-print("apple " * 5)
+import tkinter
+window = tkinter.Tk()
+window.title("我来学编程")
+window.geometry("400x300")
+lbl = tkinter.Label(window,text = "大家好啊")
+lbl.pack()
+tkinter.mainloop()
