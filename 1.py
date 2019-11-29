0f111d04df7afc4770d6b30600f572db00e5a816
wjc2008 <wjc2008@live.cn>
2019-11-29 09:23:32 +0800

    来一朵小花

---
+++ 1.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+for i in range(1,37):
+    t.colormode(255)
+    r = 255
+    g = 0
+    
+    b = 0
+    t.pencolor(r - 6 * i,6 * i,6 * i)
+
+
+    t.fd(100)
+    t.rt(90)
+
+    t.fd(100)
+    t.rt(90)
+
+    t.fd(100)
+    t.rt(90)
+
+    t.fd(100)
+    t.rt(90)
+
+    t.lt(10)
